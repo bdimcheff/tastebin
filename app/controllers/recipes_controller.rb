0f7024c3118a70889a1ee685d560f97085ffc00e
Brandon Dimcheff <bdimchef-git@wieldim.com>
2011-11-19 02:14:49 -0500

    recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,33 @@
 class RecipesController < ApplicationController
+  respond_to :html, :json
+
   def new
     @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(params[:recipe])
+
+    flash[:notice] = "Recipe was successfully created" if @recipe.save
+
+    respond_with @recipe
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+
+    respond_with @recipe
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    flash[:notice] = "Recipe was updated successfully" if @recipe.update_attributes(params[:recipe])
+
+    respond_with @recipe
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,23 @@ class Recipe
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :title
+  field :name
   field :body
 
   referenced_in :user
 
+  def body_html
+    markdown.render(body)
+  end
+
+  def safe_body_html
+    Loofah.scrub_fragment(body_html, :escape).to_s.html_safe
+  end
+
+  def markdown
+    @__markdown ||= Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML,
+      :autolink => true)
+  end
 end
 
